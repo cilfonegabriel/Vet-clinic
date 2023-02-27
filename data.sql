821967216d74739c966900d22f604281b7971826
gabicilfone <cilfonegabriel@gmail.com>
2023-02-27 18:06:19 -0300

    insert data into the animals table

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals VALUES (DEFAULT, 'Agumon', '02-03-2020', 0, TRUE, 10.23);
+INSERT INTO animals VALUES (DEFAULT, 'Gabumon', '11-15-2018', 2, TRUE, 8.0);
+INSERT INTO animals VALUES (DEFAULT, 'Pikachu', '01-07-2021', 1, FALSE, 15.04);
+INSERT INTO animals VALUES (DEFAULT, 'Devimon', '05-12-2017', 5, TRUE, 11.0);
